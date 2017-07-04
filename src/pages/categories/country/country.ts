944a5477001574a0a3abd7251e80f9d0ebaeb67e
Sebastian Kuśnierz <kusnierz.sebastian@gmail.com>
2017-07-04 19:11:23 +0200

    - remove unnecessary proxies - add loading spinner to category

--- src/pages/categories/country/country.ts
+++ src/pages/categories/country/country.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ChannelsService } from "../../../shared/channels.service";
+
 @Component( {
   selector: 'page-country',
   templateUrl: 'country.html',
@@ -8,6 +9,8 @@ import { ChannelsService } from "../../../shared/channels.service";
 export class Country implements OnInit {
   private channels: any;
   private news: any[] = [];
+  private finishedLoadingNews : boolean = false;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private channelsService: ChannelsService) {
     this.channels = navParams.get( 'channels' );
@@ -23,8 +26,10 @@ export class Country implements OnInit {
       this.channelsService.fetchNews( link ).subscribe(
         data => {
           this.news = [ ...this.news, ...data ];
-        }
-      );
+          if(index + 1 === this.channels.length ) {
+            this.finishedLoadingNews = true;
+          }
+        } );
     } );
   }
 }
